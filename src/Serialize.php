f42a6a089b5c2e71de2a9f8a0f7c0e419027a776
Mattia Basone <mattia.basone@gmail.com>
2026-01-13 00:16:25 +0100

    remove tryCatch

--- src/Serialize.php
+++ src/Serialize.php
@@ -2,7 +2,6 @@
 
 namespace FlixTech\AvroSerializer\Serialize;
 
-use Apache\Avro\AvroException;
 use Apache\Avro\Datum\AvroIOBinaryDecoder;
 use Apache\Avro\Datum\AvroIOBinaryEncoder;
 use Apache\Avro\Datum\AvroIODatumReader;
@@ -59,20 +58,16 @@ const writeDatum = '\FlixTech\AvroSerializer\Serialize\writeDatum';
 
 function writeDatum(AvroIODatumWriter $writer, AvroStringIO $io, AvroSchema $schema, mixed $record): Either
 {
-    return tryCatch(
-        static function ($record) use ($schema, $writer, $io) {
-            $io->truncate();
-            $writer->writeData($schema, $record, avroBinaryEncoder($io));
+    try {
+        $io->truncate();
+        $writer->writeData($schema, $record, avroBinaryEncoder($io));
 
-            return Right::of($io->string());
-        },
-        static function (AvroException $e) use ($record, $schema) {
-            return Left::of(
-                Exceptions::forEncode($record, $schema, $e)
-            );
-        },
-        $record
-    );
+        return Right::of($io->string());
+    } catch (\Throwable $exception) {
+        return Left::of(
+            Exceptions::forEncode($record, $schema, $exception)
+        );
+    }
 }
 
 const avroDatumReader = '\FlixTech\AvroSerializer\Serialize\avroDatumReader';
@@ -97,19 +92,15 @@ function readDatum(
     AvroSchema $readersSchema,
     mixed $data,
 ): Either {
-    return tryCatch(
-        static function ($data) use ($writersSchema, $readersSchema, $reader, $io) {
-            $io->truncate();
-            $io->write($data);
-            $io->seek(0);
+    try {
+        $io->truncate();
+        $io->write($data);
+        $io->seek(0);
 
-            return Right::of($reader->readData($writersSchema, $readersSchema, avroBinaryDecoder($io)));
-        },
-        static function (\Throwable $e) use ($data) {
-            return Left::of(
-                Exceptions::forDecode($data, $e)
-            );
-        },
-        $data
-    );
+        return Right::of($reader->readData($writersSchema, $readersSchema, avroBinaryDecoder($io)));
+    } catch (\Throwable $exception) {
+        return Left::of(
+            Exceptions::forDecode($data, $exception)
+        );
+    }
 }
